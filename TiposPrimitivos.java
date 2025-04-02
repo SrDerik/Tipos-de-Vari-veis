b48d8ec6e93990a60370b9eaa5e1833fcb9dcfb8
Derik Soares <deriksoares217@gmail.com>
2025-04-02 09:29:01 -0300

    Update TiposPrimitivos.java

--- TiposPrimitivos.java
+++ TiposPrimitivos.java
@@ -6,18 +6,18 @@ package com.mycompany.tiposprimitivos2;
 
 /**Eu Derik S. estou fazendo meu primeiro algoritmo
  *
- * @author FATEC ZONA LESTE
+ * @author D3
  */
 public class TiposPrimitivos {
     public static void main(String[] args){
-       // Lê o que está dentro da variável primitiva
-       byte idade = 25;  // Recebe 2 valores, é necessário fechar com ; ;Armazena números pequenos (-128 a 127)
-       short ano =2024; //Pode ter até 4 casas(como um ano), ficha com ;Armazena valores um pouco maiores (-32.768 a 32.767)
-       int populacaoCidade = 500000; //Variável inteira recebe números inteiros ;Tipo Mais comum para números inteiros
-       long populacaoMundial = 8000000000L; //Necessário'L' para valores longos
+       // Lê o que está dentro da variável 
+       byte idade = 25;  // Recebe até 2 valores.                                                                                   ;Explicação do professor:"Armazena números pequenos (-128 a 127)"
+       short ano =2024; //Pode ter até 4 casas(como um ano).                                                                        ;Explicação do professor:"Armazena valores um pouco maiores (-32.768 a 32.767)"
+       int populacaoCidade = 500000; //Variável inteira recebe números inteiros.                                                    ;Explicação do professor:"Tipo Mais comum para números inteiros"
+       long populacaoMundial = 8000000000L; //Necessário'L' para valores longos                                                     ;Explicação do professor:
        
-       float altura = 1.75f; //Pode colocar números com casa decimal, tem que usar o . ;Necessário 'F' para indicar
-       double peso = 72.5; //Pode colocar números com casa decimal, tem que o . ;Mais preciso que float
+       float altura = 1.75f; //Pode colocar números com casa decimal, tem que usar o F, . e , (;)                                   ;Explicação do professor:"Necessário 'F' para indicar"
+       double peso = 72.5; //Pode colocar números com casa decimal e é mais preciso que o 'float', tem que o . e , (;)              ;Explicação do professor:"Mais preciso que float"
        
        char inicial = 'J'; //Armazena um único caractere, tem que usar aspas
        boolean ativo = true; //Armazena dois valores, sendo eles verdadeiros ou falsos
@@ -25,7 +25,7 @@ public class TiposPrimitivos {
        //Exibição dos valores
        System.out.println ("Idade: " + idade ); //É necessário usar aspas duplas "
        System.out.println ("Ano: " + ano ) ;
-       System.out.println ("População da cidade: " + populacaoCidade) ;
+       System.out.println ("População da cidade: " + populacaoCidade) ;                                                                                      // TUDO FECHA COM ;           
        System.out.println ("População mundial: " + populacaoMundial) ;
        System.out.println ("Altura: " + altura + "m") ;
        System.out.println ("Peso: " + peso + "kg") ;
